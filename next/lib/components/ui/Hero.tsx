0a3a8dc76a5b3ede44cbdec73f91420e738d94c3
Philip Schmidt <philip.schmidt@networkteam.com>
2023-03-17 13:56:45 +0100

    fix hero / fix logo

--- next/lib/components/ui/Hero.tsx
+++ next/lib/components/ui/Hero.tsx
@@ -19,7 +19,12 @@ const Hero = ({ title, subtitle, description, image, className }: HeroProps) => 
   const landscape = image && image.width > image.height;
 
   return (
-    <section className={classNames('container mx-auto flex flex-col gap-8 px-4 md:flex-row lg:gap-12', className)}>
+    <section
+      className={classNames(
+        'container mx-auto flex min-h-[calc(100vh_-_10rem)] flex-col justify-between gap-8 px-4 py-8 md:flex-row lg:gap-12 lg:py-16',
+        className
+      )}
+    >
       <div
         className={classNames('justify-self-end text-dark dark:text-white lg:mb-0', {
           'lg:mt-24 xl:mt-48 2xl:mt-64': landscape,
@@ -27,14 +32,16 @@ const Hero = ({ title, subtitle, description, image, className }: HeroProps) => 
         })}
       >
         <h1 className="font-bayon text-[10rem] leading-tight lg:w-2/3 xl:w-1/2">{title}</h1>
-        <div className="mb-4 font-bayon text-2xl lg:w-2/3">{subtitle}</div>
-        <div
-          className={classNames('text-lg', {
-            'md:-mr-48 lg:-mr-32': landscape,
-          })}
-        >
-          {description}
-        </div>
+        {subtitle && <div className="mb-4 font-bayon text-2xl lg:w-2/3">{subtitle}</div>}
+        {description && (
+          <div
+            className={classNames('text-lg', {
+              'md:-mr-48 lg:-mr-32': landscape,
+            })}
+          >
+            {description}
+          </div>
+        )}
       </div>
       <div
         className={classNames('flex shrink-0 justify-center', {

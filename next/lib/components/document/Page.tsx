0787b85f723778dad31c6d7275ff78417bfc718f
Christopher Hlubek <christopher.hlubek@networkteam.com>
2023-05-03 16:58:46 +0200

    Update Networkteam.Neos.ContentApi and @networkteam/zebra

--- next/lib/components/document/Page.tsx
+++ next/lib/components/document/Page.tsx
@@ -1,11 +1,8 @@
-import { ContentCollection, useInBackend, useNode } from '@networkteam/zebra';
+import { ContentCollection } from '@networkteam/zebra';
 
 import Layout from './partials/Layout';
 
 const DocumentPage = () => {
-  const node = useNode();
-  const inBackend = useInBackend();
-
   return (
     <Layout>
       <ContentCollection nodeName="main" />
